ea93722dfbe8cc311f2b8ccbb2f85a924a29fa1e
Evan <ejdoerks@ualberta.ca>
2023-10-03 19:13:28 -0600

    Update Octahedron.java

--- app/src/main/java/com/example/cmput301f23t09/Octahedron.java
+++ app/src/main/java/com/example/cmput301f23t09/Octahedron.java
@@ -3,6 +3,6 @@ package com.example.cmput301f23t09;
 public class Octahedron extends Shape{
     Octahedron() {
         super();
-        String color = "purple";
+        String color = "green";
     }
 }
